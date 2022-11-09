d49b648a5b37f34b02d6b9999f0bba0c81705440
“NAGA222” <bhushanam2222@gmail.com>
2022-11-09 15:13:57 +0530

    terraform installtion cmds added

--- Jenkins_as_a_service_installation_on_ubuntu-server.sh
+++ Jenkins_as_a_service_installation_on_ubuntu-server.sh
@@ -11,3 +11,9 @@ apt-get update
 apt-get install jenkins -y
 systemctl start jenkins
 systemctl status jenkins
+
+#terraform on jenkins server
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+terraform -version
